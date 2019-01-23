a9a0953d2f71492f7f878e398b69336d02818963
Andrew Teta <andrew.teta@gmail.com>
2019-01-23 16:57:50 -0700

    Finished thru #4

--- Lab1/Lab1/Lab1.py
+++ Lab1/Lab1/Lab1.py
@@ -6,8 +6,10 @@ Created on Wed Jan 16 15:24:09 2019
 """
 
 import scipy.io.wavfile
+import scipy.signal
 import numpy as np
 import matplotlib.pyplot as plt
+from tkinter import filedialog
 
 # =============================================================================
 # filepath = string containing absolute path to audio file
@@ -34,15 +36,70 @@ def loudness(sample,N):
         E[n] = (1/N)*sum(sample[n*N:n*N+(N-1)])
     for n in range(nframes):
         sigma[n] = np.sqrt((1/N)*sum((sample[n*N:n*N+(N-1)] - E[n])**2))
-    return E,sigma
+    return E,sigma,nframes
 
-filepath = 'audio/track201-classical.wav'
-fs,songSample = exSample(filepath,24,120)
-E,sigma = loudness(songSample,512)
+def zcr(sample,N):
+    nframes = int(len(sample)/N)
+    Z = np.zeros(nframes)
+    for n in range(nframes):
+        Z[n] = 1/(N-1) * sum(0.5*abs(np.sign(sample[n*N+1:n*N+(N-1)]) - np.sign(sample[n*N:n*N+(N-2)])))
+    return Z, nframes
+
+files = filedialog.askopenfilenames()
+for f in files:
+    filename = str.split(f,'/')[-1]
+    filename = str.split(filename,'.')[0]
+
+    filepath = f
+    fs,songSample = extractSample(filepath,24,120)
+    E,sigma,Nloudness = loudness(songSample,512)
+    Z,Nzcr = zcr(songSample,512)
+
+    plt.figure()
+    plt.plot(range(Nloudness),sigma)
+    plt.xlabel('frames')
+    plt.ylabel('loudness')
+    plt.title('Loudness: '+filename)
+    plt.savefig('figs/loudness_'+filename)
+    plt.close()
+
+    plt.figure()
+    plt.plot(range(Nzcr),Z)
+    plt.xlabel('frames')
+    plt.ylabel('ZCR')
+    plt.title('ZCR: '+filename)
+    plt.savefig('figs/zcr_'+filename)
+    plt.close()
+
+w = scipy.signal.get_window('hann',512)
 plt.figure()
-plt.plot(sigma)
-plt.show()
+plt.plot(w)
+plt.title('Hann Window')
+plt.xlabel('Samples')
+plt.ylabel('Magnitude')
+plt.savefig('figs/hann')
+plt.close()
+w = scipy.signal.get_window('blackman',512)
+plt.figure()
+plt.plot(w)
+plt.title('Blackman Window')
+plt.xlabel('Samples')
+plt.ylabel('Magnitude')
+plt.savefig('figs/blackman')
+plt.close()
 
+def FFT(sample,fs,N):
+    nframes = int(len(sample)/N)
+    w = scipy.signal.get_window('blackman',N-1)
+    for n in range(nframes):
+        s = sample[n*N:n*N+(N-1)]
+        f,t,s = scipy.signal.spectrogram(sample[n*N:n*N+(N-1)],fs,w)
+        plt.figure()
+        plt.plot(f,s)
+        plt.show()
+
+FFT(songSample,fs,512)
+    
 
 
 
