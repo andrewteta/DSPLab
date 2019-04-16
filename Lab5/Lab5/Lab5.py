1926a3079c11b8526f4f13819756b5f46a2eb5c3
Andrew Teta <andrew.teta@gmail.com>
2019-04-15 20:01:26 -0600

    Implemented dctmgr and idctmgr.
    Started report and generated some test images.

--- Lab5/Lab5/Lab5.py
+++ Lab5/Lab5/Lab5.py
@@ -7,7 +7,14 @@ import lab_functions as lf
 # UI dialog to select files -> selection of multiple files will run all functions for each file
 files = filedialog.askopenfilenames()
 for f in files:
+    filename = str.split(f,'/')[-1]
+    filename = str.split(filename,'.')[0]
+    filepath = f
     image = np.asarray(Image.open(f))
-    processed = lf.dctmgr(image)
+    Image.fromarray(image).save('figures/' + filename + '.png')
+    loss_factor = 8
+    processed = lf.dctmgr(image, loss_factor)
+    x = lf.idctmgr(processed, loss_factor)
+    Image.fromarray(x.astype(np.uint8)).save('figures/' + filename + '_lf' + str(loss_factor) + '.png')
 
 print('done')
--- Lab5/Lab5/lab_functions.py
+++ Lab5/Lab5/lab_functions.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import scipy.fftpack
+from PIL import Image
 
 zz = np.asarray([
 [0, 0],
@@ -37,8 +38,8 @@ zz = np.asarray([
 [3, 4],
 [4, 3],
 [5, 2],
-[5, 1],
-[6, 0],
+[6, 1],
+[7, 0],
 [7, 1],
 [6, 2],
 [5, 3],
@@ -79,56 +80,133 @@ Q = np.asarray([
         [72, 92, 95, 98, 112, 100, 103, 99] ], np.float)
 
 def dct_2D(A):
+    #print(f'before = \n{A}\n')
     X = scipy.fftpack.dct(A, axis = 0, norm='ortho')
     Y = scipy.fftpack.dct(X, axis = 1, norm='ortho')
+    #print(f'dct = \n{Y}\n')
     return Y
 
 def idct_2D(A):
     Y = scipy.fftpack.idct(A, axis = 0, norm='ortho')
     X = scipy.fftpack.idct(Y, axis = 1, norm='ortho')
+    #print(f'after = \n{X}\n')
     return X
 
 def zigzag(A):
-    print(f'before zigzag = \n{A}\n\n')
+    #print(f'before zigzag = \n{A}\n\n')
     Y = np.zeros(64)
     for n in range(len(zz)):
         map = zz[n]
         Y[n] = A[map[0], map[1]]
-    print(f'after zigzag = \n{Y}\n\n')
+    #print(f'after zigzag = \n{Y}\n\n')
     return Y
 
 def izigzag(A):
+    #print(f'before inverse zigzag = \n{A}\n\n')
+    X = np.zeros((8, 8))
+    for n in range(len(A)):
+        map = zz[n]
+        X[map[0], map[1]] = A[n]
+    #print(f'after inverse zigzag = \n{X}\n\n')
     return X
 
-def dctmgr(image):
+def dctmgr(image, loss_factor):
     processed_image = np.zeros_like(image)
-    dct_array = np.zeros((64, (int)(np.shape(image)[0] * np.shape(image)[1] / 64)))
+    dct_array = np.zeros((64, int(np.shape(image)[0] * np.shape(image)[1] / 64)))
     blockSize = 8
     Ny, Nx = np.shape(image)
     nBlocksX = int(Nx/blockSize)
     nBlocksY = int(Ny/blockSize)
+    block = 0
+    #print(f'block before processing = \n{image[0:8,0:8]}\n')
+    #print(f'block after 2d dct = \n{dct_2D(image[0:8,0:8])}\n')
     # loop over 8x8 block cols
-    for blockX in range(nBlocksX):
-        for blockY in range(nBlocksY):
+    for row in range(nBlocksX):
+        for col in range(nBlocksY):
+            indexX = col * 8
+            indexY = row * 8
+            #print(f'indexX (col) = {indexX}')
+            #print(f'indexY (row) = {indexY}')
             # slice out an 8x8 block
-            pix = image[blockY : blockY + blockSize, blockX : blockX + blockSize]
+            pix = image[indexY : indexY + blockSize, indexX : indexX + blockSize]
+            #print(f'input = \n{pix}\n')
             # take 2D DCT transform
             dct_pix = dct_2D(pix)
-            zig = zigzag(pix)
-            # calculate and place block in zig-zag pattern location
-    return processed_image
+            #print(f'dct = \n{dct_pix}\n')
+            # quantize
+            q = quant_coeffs(dct_pix, loss_factor)
+            #print(f'q = \n{q}\n')
+            # re-order block in zigzag pattern and place in output array
+            zz = zigzag(q)
+            dct_array[:, block] = zz
+            #print(f'zz = \n{zz}\n')
+            #print(f'dct_array[i] = \n{dct_array[:, block]}\n')
+            #iz = izigzag(zz)
+            #print(f'izz = \n{iz}\n')
+            #iq = iquant_coeffs(iz, 1)
+            #print(f'iq = \n{iq}\n')
+            #y = idct_2D(iq)
+            #print(f'idct = \n{y}\n')
+            #print(f'block = {block}')
+            block += 1
+    # quantize values according to Q and loss factor
+    #output = quant_coeffs(dct_array, loss_factor)
+    output = dct_array
+    #print(f'first column after compression = \n{output[:,0]}\n')
+    return output
 
-# rows * cols / 64 = numblocks
-# make this an array with 64 rows
+def idctmgr(input, loss_factor):
+    nPix = int(np.shape(input)[1] / 8)
+    output = np.zeros((nPix, nPix))
+    #coeffs = input
+    for col in range(np.shape(input)[1]):
+        #print(f'input col = \n{input[:,col]}\n')
+        indexX = (col % 64) * 8
+        indexY = (int(col / 64)) * 8
+        #print(f'index (row={indexX},col={indexY})\n')
+        zag = izigzag(input[:, col])
+        # invert quantization
+        coeffs = iquant_coeffs(zag, loss_factor)
+        #print(f'izz = \n{zag}\n')
+        idct = idct_2D(coeffs)
+        #print(f'idct = \n{idct}\n')
+        output[indexY:indexY + 8, indexX:indexX + 8] = idct
+        #Image.fromarray(output).show()
+        #print('\n')
+    return output
 
+def quant_coeffs(A, loss_factor):
+    #print(f'before quant = \n{A[0:8,0:8]}\n')
+    #A[1:,:] = np.floor((np.divide(A[1:,:], zigzag(Q)[1:, np.newaxis]) / loss_factor) + 0.5)
+    z = A[0,0]
+    A = np.floor((A / (loss_factor * Q)) + 0.5)
+    A[0,0] = z
+    #print(f'after quant = \n{A[0:8,0:8]}\n')
+    return A
 
-def quant_coeffs(A):
-    return Y
-
-def iquant_coeffs(A):
-    return X
+def iquant_coeffs(A, loss_factor):
+    #A[1:,:] = A[1:,:] * loss_factor * zigzag(Q)[1:, np.newaxis
+    z = A[0,0]
+    A = A * loss_factor * Q
+    A[0,0] = z
+    #print(f'after iquant = \n{A[0:8,0:8]}\n')
+    return A
 
 def enc_rbv(A):
+    symb = np.zeros( (0,3), np.int )
+    for i in range(np.shape(A)[1]):
+        # Handle DC coeff
+        symb = np.vstack( [symb, [0, 12, A[0,i]]] )
+
+        # Handle AC coeffs. nzi means non-zero indices
+        tmp = A[:,i].flatten()
+        nzi = np.where( tmp[1:] != 0 ) [0]
+        prev_index = 0
+        for k in range(len(nzi)):
+            curr_index = nzi[k] + 1
+            zeros = curr_index - prev_index - 1
+            symb = np.vstack([symb, [0,0,0]])
+        return symb
     return Y
 
 def dec_rbv(A):
